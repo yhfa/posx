48db668f5ed33d1df3b37a383e0fef567cf54403
yhf <youssifhany75@gmail.com>
2023-04-01 21:49:20 +0200

    add: not-found page

---
+++ src/pages/notFound.tsx
@@ -0,0 +1,18 @@
+import { Box, Flex, Image, Link, Text, VStack } from '@chakra-ui/react';
+import { Link as ReactRouterLink } from 'react-router-dom';
+import notFound from '../assets/not-found.avif';
+
+function NotFound() {
+  return (
+    <VStack w={96} m="auto" align="center" justify="center">
+      <Image src={notFound} />
+      <Box>
+        <Link as={ReactRouterLink} to="/">
+          <Text>Return to home</Text>
+        </Link>
+      </Box>
+    </VStack>
+  );
+}
+
+export default NotFound;
