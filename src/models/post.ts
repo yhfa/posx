af983f37c0aa1b2d06bdb8fff03455b3591a33ac
yhf <youssifhany75@gmail.com>
2023-04-01 21:49:05 +0200

    update: post interface

--- src/models/post.ts
+++ src/models/post.ts
@@ -2,18 +2,20 @@ import { Timestamp } from 'firebase/firestore';
 
 export interface Post {
   id: string;
-  userId: string;
   createdAt: Date;
   title: string;
   body: string;
   imgUrl?: string;
+  userImg: string;
+  userName: string;
 }
 
 export interface BackendPost {
   id: string;
-  userId: string;
   created_at: Timestamp;
   title: string;
   body: string;
   img_url?: string;
+  user_img: string;
+  user_name: string;
 }
